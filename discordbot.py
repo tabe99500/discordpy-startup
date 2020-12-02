0cdb03f61247c5b2241c9df65aa9ff00bd240803
tabe99500 <75172676+tabe99500@users.noreply.github.com>
2020-12-02 18:10:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,6 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 # 返信する非同期関数を定義
-@client.event
 async def reply(message):
     reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
     await message.channel.send(reply) # 返信メッセージを送信
