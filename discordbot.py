df6bfec403d5dcd352ca4600adb6d7b89e023254
tabe99500 <75172676+tabe99500@users.noreply.github.com>
2020-12-02 17:22:47 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,6 +15,10 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send('ぴよよ')
 
+@bot.command()
+async def neko(ctx):
+    await ctx.send('ぴよよ～')
+    
 bot.run(token)
