0e9ba1ab3f1540454aebacf803fca9b908e874bc
tabe99500 <75172676+tabe99500@users.noreply.github.com>
2021-01-01 00:53:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,5 +8,26 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.command(neme='ひよこ')
 async def ひよこ(ctx):
     await ctx.send('ぴよよ～')
+
+@bot.command(name='疲れた')
+async def 疲れた(ctx):
+    await ctx.send('ぴよ？？？？？')
     
+@bot.command(name='よりくん')
+async def よりくん(ctx):
+    await ctx.send('ぴよっ💓')
+    
+@bot.command(name='@ひよこまる')
+async def ひよこまる(ctx):
+    await ctx.send('ぴよよ🐥')    
+
+@bot.command(name=os.environ['DISCORD_BOT_ID'])
+async def name(ctx):
+    await ctx.channel.send('ぴよよ🐥')
+    
+@bot.command(name=os.environ['DISCORD_BOT_ID_PHONE'])
+async def name(ctx):
+    await ctx.channel.send('ぴよよ🐥')
+    
+
 bot.run(token)
